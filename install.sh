43ce8e343358cc773169b78dc1b8b43c703a9d11
DerbanTW <derbanthul@gmail.com>
2019-11-28 22:04:57 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo Install Script started...
+
+apt-get update
+
+apt install locales-all
+
+apt-get install screen
+
+apt-get install tmux
+
+apt-get install htop
+
+apt-get install default-jdk
+
+apt-get update
+
+echo Install Script finished
