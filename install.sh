e6e014681c687b60b683ca44aed375fc6b56a887
Derban <derbanthul@gmail.com>
2019-12-11 23:47:31 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Works for Debian 9.4
-#wget -P /home/bashscripts/ https://raw.githubusercontent.com/DerbanTW/bash/master/install.sh
-#chmod +x ./install.sh
+#wget -P /home/bashscripts/ https://raw.githubusercontent.com/DerbanTW/bash/master/install.sh && chmod +x /home/bashscripts/install.sh
+#./install.sh
 #You have to confirm with Yes/No
 
 echo Install Script started...
