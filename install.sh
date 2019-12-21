3f926684075c36403c804ca31e7fab0522e5575f
Derban <derbanthul@gmail.com>
2019-12-21 01:44:50 +0100

    acc Name change

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Works for Debian 9.9 (minimal)
-# wget --no-check-certificate -P /YOUR_DIRECTORY/DBTerminal/ https://raw.githubusercontent.com/DerbanTW/DBTerminal/master/install.sh && chmod +x /YOUR_DIRECTORY/DBTerminal/install.sh
+# wget --no-check-certificate -P /YOUR_DIRECTORY/DBTerminal/ https://raw.githubusercontent.com/DerbanTC/DBTerminal/master/install.sh && chmod +x /YOUR_DIRECTORY/DBTerminal/install.sh
 # ./install.sh
 
 
