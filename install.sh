b5d54dd806fbd7240965b7f46266d76435c4bc12
Derban <derbanthul@gmail.com>
2020-01-07 21:40:26 +0100

    add auto-connection to tmux by login

--- install.sh
+++ install.sh
@@ -71,23 +71,26 @@ setLocalesDE() {
 # Some installations couldn't read the scripts; first solution.
 fixBashrc() {
 	fixPath="export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+	autoConnect='if ! [[ -z $(tmux ls | grep -o Terminal) ]];then tmux a -t Terminal; fi'
+	CatchautoConnect='tmux a -t Terminal'
 	bashrc="/root/.bashrc"
-	tmpfile=instTempfile
 	if [[ -f $bashrc ]];then
 		while read -r line || [ -n "$line" ]; do 
 			if [[ $line == $fixPath ]];then
-				echo exportPathFound > instTempfile
+				exportPathFound=true
+			elif [[ $line =~ $CatchautoConnect ]];then
+				autoConnectFound=true
+				echo autoConnect found lol
 			fi
 		done < $bashrc
 	fi
-
-	if [[ -f $tmpfile ]];then
-		if ! [[ -z $(grep -o 'exportPathFound' $tmpfile) ]];then
-			rm $tmpfile
-		fi
-	else
-		( echo "" ; echo "$fixPath" ; echo "" ) >> $bashrc
+	if ! [[ $exportPathFound == true ]];then
+		echo -e "$fixPath" >> $bashrc
 		echo -e "[Done/fixBashrc]: -> Linie [$fixPath] wurde der Datei <$bashrc> hinzugefuegt!"
+	fi
+	if ! [[ $autoConnectFound == true ]];then
+		echo -e "$autoConnect" >> $bashrc
+		echo -e "[Done/fixBashrc]: -> Linie [$autoConnect] wurde der Datei <$bashrc> hinzugefuegt!"
 	fi
 }
 
